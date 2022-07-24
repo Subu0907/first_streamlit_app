2da64a34c10acbecd76e36a0c4a9614e47daf23c
Subhransu Jena <109449403+Subu0907@users.noreply.github.com>
2022-07-24 17:19:45 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -47,4 +47,4 @@ streamlit.text(my_data_row)
 
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe (my_data_row)
+streamlit.dataframe (my_data_rows)
