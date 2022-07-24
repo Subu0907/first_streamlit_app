c840d9f685ec7756a0088b87c7c672aa39dd76b1
Subhransu Jena <109449403+Subu0907@users.noreply.github.com>
2022-07-24 16:27:51 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -36,6 +36,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+import snowflake.connector.snowflake-connector-python
+# requirements.txt
+
    
 
