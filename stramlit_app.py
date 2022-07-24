4074760aaa10576bf075d5b8f1d50b7d00586da7
Subhransu Jena <109449403+Subu0907@users.noreply.github.com>
2022-07-24 17:29:47 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -46,7 +46,9 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-import pandas
+
+streamlit.dataframe(fruits_to_show)
+
 streamlit.header("Fruityvice Fruit Advice!")
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', add_my_fruit)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
